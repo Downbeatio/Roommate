bd79bdb208701e473d9a6cfe66f45fc071cb5e83
Michael Cutrone <michael.cutrone@loop.colum.edu>
2015-12-18 13:24:33 -0600

    Added rooomate_id to item_params

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -69,6 +69,6 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:name, :price)
-    end
+      params.require(:item).permit(:name, :price, :roommate_id)
+    end 
 end
